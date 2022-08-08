2df3863ec06a671ac3810942891a04dc22f4839b
= <hudson@kenzie.com.br>
2022-08-08 11:12:33 -0300

    introducao js

---
+++ JavaScript/01-introducao-javaScript/js/script.js
@@ -0,0 +1,64 @@
+//console.log("Olá Mundo")
+
+//VARIAVEIS 
+// let nomePessoa = "Hudson"
+// let numero     = 10
+
+// numero = "Anderson"
+
+// //
+//const numero1 = 50
+
+//TIPOS DE DADOS
+//INTEIROS => 10    
+let numero = 10
+let numero2 = 10.50
+
+//STRINGS 
+let texto = "Hudson"
+let textoNumero = "10"
+
+//boolean
+let verdadeiro  = true
+let falso       = false 
+
+//OPERADORES LÓGICOS
+
+//IGUALDADE      10 == 10 => TRUE 
+//IGUALDADE      10 == 20 => FALSE 
+
+//IGUALDADE+TIPO 10 === 10 => TRUE 
+//IGUALDADE+TIPO "10" === 10 => FALSE 
+
+//MAIOR QUE      10 > 9 => TRUE 
+//MENOR QUE      10 < 9 => FALSE 
+
+//MAIOR IGUAL    10 >= 10 => TRUE  
+//MENOR IGUAL    10 <= 10 => TRUE  
+
+//DIFERENTE      10 !== 20  => true 
+//DIFERENTE      10 !== 10  => false 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
