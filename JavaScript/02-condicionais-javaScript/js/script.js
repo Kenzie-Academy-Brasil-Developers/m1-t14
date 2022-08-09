d598eedc7505479d914f8012dcb72ecaadcb0c0a
= <hudson@kenzie.com.br>
2022-08-09 11:30:28 -0300

    introducao condicionais

---
+++ JavaScript/02-condicionais-javaScript/js/script.js
@@ -0,0 +1,78 @@
+//OPERADORES ARITIMETICOS
+// +  SOMA
+// - SUBTRAÇÃO
+// * MULTIPLICAÇÃO 
+// / DIVISÃO
+// (++) (+=) INCREMENTO 
+//  --  (-=) DECREMENTO 
+// numero = numero + 1
+// numero = numero - 1
+
+// CONDICIONAIS
+
+// let idade = 18 
+
+// //SE(CONDICIONAL)
+
+// //SE
+// if(idade >= 50){
+//     console.log("Atingiu maior idade")
+
+// //SENÃO
+// }else{
+//     console.log("Não atingiu maior idade")
+// }
+
+
+
+//let usuario = "Aderson" // "hudson@gmail.com"
+//let senha   = "1234"
+
+// EE == &&
+// OU == ||
+
+
+//if(usuario == "hudson" || usuario == "hudson@gmail.com")
+
+
+//USUARIO => TRUE                      //SENHA => TRUE
+// if((usuario == "Hudson" || usuario == "Aderson") && senha == "1234"){
+    
+//     console.log('Login sucesso!')
+
+//     if(usuario == "Hudson"){
+//         console.log("Acesso admin")
+//     }
+
+// }else{
+//     console.log('Usuário ou senha invalido!')
+// }
+
+
+
+//VERIFICANDO NUMERO PAR/IMPAR
+// let numero = prompt("Digite um numero")
+
+// if(numero % 2 == 0){
+//     alert("Numero par")
+// }else{
+//     alert("Numero impar")
+// }
+
+// let numero1 = +prompt("Digite o primeiro numero")
+// let numero2 = +prompt("Digite o segundo numero")
+
+// let numero1 = prompt("Digite o primeiro numero")
+// let numero2 = prompt("Digite o segundo numero")
+
+// //NUMBER() => CONVERTE UMA STRING PARA INTEIRO(NUMERO)
+
+// let result = Number(numero1) + Number(numero2)
+// console.log(result)
+
+
+
+
+
+
+
