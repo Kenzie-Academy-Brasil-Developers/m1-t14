f24e0910662ed61defa067619234204876b20201
= <hudson@kenzie.com.br>
2022-08-10 10:41:21 -0300

    avançando com condicionais

--- JavaScript/02-condicionais-javaScript/js/script.js
+++ JavaScript/02-condicionais-javaScript/js/script.js
@@ -12,16 +12,13 @@
 
 // let idade = 18 
 
-// //SE(CONDICIONAL)
+// // //SE(CONDICIONAL)
 
-// //SE
-// if(idade >= 50){
+// // //SE
+// if(idade >= 18){
 //     console.log("Atingiu maior idade")
-
-// //SENÃO
-// }else{
-//     console.log("Não atingiu maior idade")
 // }
+
 
 
 
@@ -65,7 +62,7 @@
 // let numero1 = prompt("Digite o primeiro numero")
 // let numero2 = prompt("Digite o segundo numero")
 
-// //NUMBER() => CONVERTE UMA STRING PARA INTEIRO(NUMERO)
+//NUMBER() => CONVERTE UMA STRING PARA INTEIRO(NUMERO)
 
 // let result = Number(numero1) + Number(numero2)
 // console.log(result)
@@ -73,6 +70,93 @@
 
 
 
+// let numero1    = prompt("Digite o primeiro numero")
+// let numero2    = prompt("Digite o segundo numero")
+// let resultado  = 0
+
+// //TRUE             FALSE
+// if(numero1 > 0 && numero2 > 0){
+    
+//     //CONVERTENDO PARA DEPOIS FAZER A SOMA
+//     resultado = Number(numero1) + Number(numero2)
+//     alert(resultado)
+
+// //SENÃO
+// }else{
+
+//     alert("Precisa ser numeros maiores que 0")
+// }
 
 
 
+
+//CALCULADORA
+//VALOR-1+
+//OPERAÇÃO 
+//RESULTADO
+
+//1) RECUPERAR OPERAÇÃO DO USUÁRIO **
+//2) PRIMEIRO NUMERO **
+//3) SEGUNDO NUMERO **
+//4) CONDICIONAL PARA 
+//IDENTIFICAR QUAL A OPERAÇÃO
+//5) FAZER O CALCULO 
+//6) APRESENTAR RESULTADO 
+
+
+// let operacao   = prompt("Digite a operação (+,-,*,/)")
+// let numero1    = prompt("Digite o primeiro numero")
+// let numero2    = prompt("Digite o segundo numero")
+// let resultado  = 0
+// console.log(operacao)
+// console.log(numero1)
+// console.log(numero2)
+
+
+// //+ OU SOMA
+// if(operacao == "+" || operacao == "soma"){
+    
+//     resultado = Number(numero1) + Number(numero2)
+//     console.log(resultado)
+
+// }
+
+// if(operacao == "-" || operacao == "subtrac"){
+
+//     resultado = Number(numero1) - Number(numero2)
+//     console.log(resultado)
+
+// }
+
+// if(operacao == "*" || operacao == "multiplicação"){
+
+//     resultado = Number(numero1) * Number(numero2)
+//     console.log(resultado)
+
+// }
+
+
+// if(operacao == "/"){
+
+//     resultado = Number(numero1) / Number(numero2)
+//     console.log(resultado)
+
+// //SENÃO
+// }else{
+//     console.log("Operação invalida")
+// }
+
+
+
+
+//DESCONTO NO FRETE 
+// let totalCompra = 200
+// let valorFrete  = 50
+// let total       = 0
+// if(totalCompra >= 200){
+
+//         total = totalCompra + (valorFrete - 25) 
+    
+// }else if(totalCompra > 500){
+//     total = total = totalCompra + (valorFrete - valorFrete)
+// }
