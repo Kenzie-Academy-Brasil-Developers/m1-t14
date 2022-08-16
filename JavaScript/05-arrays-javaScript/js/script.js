cd02888fff6541c035babc44c8411780e92a5ce1
= <hudson@kenzie.com.br>
2022-08-16 10:32:35 -0300

    Introducão a arrays

---
+++ JavaScript/05-arrays-javaScript/js/script.js
@@ -0,0 +1,70 @@
+
+//1) ARRAY COM VALORES JÁ DEFINIDO
+
+//let nomes = ["Hudson", "Anderson", "Daniel", "André"]
+
+
+//ARRAY VAZIO
+//let numeros = [1,2,3,34,54,65]
+
+
+//2) COMO ACESSAR UM AVLOR
+//console.log(nomes[3])
+
+
+//3) ALTERAR VALROES DENTRO DE UM ARRAY
+
+//"Daniel" => Ugo
+// nomes[2] = "Ugo"
+// nomes[0] = "Gabriel"
+// nomes[3] = 12 + 23
+
+//4) Adicionando um novo valor
+
+//nomes.push("Ugo", "Gabriel", "Lucas")
+//
+//let nomeCompleto = "Hudson Carolino"
+//nomes.push(nomeCompleto)
+
+
+
+
+
+//LOOP
+//ENQUANTO(condicional = true){
+    
+    //......
+
+//}
+
+
+
+let nomes = ["Hudson", "Anderson", "Daniel", "André", "Lucas"]
+
+
+let contador  =  0
+
+while(contador < nomes.length){
+
+
+    console.log("Nome: "+nomes[contador])
+
+
+    contador = contador + 2
+}
+
+
+// let arrayDados  =  []
+
+// while(contador <= 10){
+
+//     let dado  = prompt("Digite um dado")
+//     arrayDados.push(dado)
+   
+
+//     contador++
+// }
+
+
+
+
